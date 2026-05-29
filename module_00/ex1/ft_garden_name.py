940ca1000497992e48ce7e5f5af34d26c2269ec4
nnomenja <mamenosoa42@gmail.com>
2026-05-29 15:43:34 +0300

    fix lint error

--- module_00/ex1/ft_garden_name.py
+++ module_00/ex1/ft_garden_name.py
@@ -1,4 +1,4 @@
 def ft_garden_name() -> None:
-        name: str = input("Enter garden name: ")
-        print("Garden: ", name)
-        print("Status: Growing well!")
+    name: str = input("Enter garden name: ")
+    print("Garden: ", name)
+    print("Status: Growing well!")
